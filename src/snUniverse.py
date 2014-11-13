cbfe31536d54adc9f34cee2252f85f33d3fa083a
Rahul Biswas <rbiswas4@gmail.com>
2014-11-12 16:27:36 -0800

    First step towards refactoring the instance class into a class that describes the SN population and an instance class. snUniverse.py: class which describes the properties of SNIa in the universe in terms of a model, and in terms of the distribution functions the parameters of that model take. It would be good to define an abstract class which does this in a general case and put what I have now in a specific SALT2-universe class which inherits from that class.
    test_snUniverse.ipynb: an ipython notebook to test that this class is
    working correctly.

---
+++ src/snUniverse.py
@@ -0,0 +1,53 @@
+"""
+Class describing implementation of a SN universe specified by the following
+attributes and methods:
+
+    atrributes:
+        SNModel: Physically this is a phenomenological model of SN such as\
+            the SALT2 model. In practice, this could point to an SNCosmo Model\
+            object.
+    methods:
+        SNdists: This specifies how the parameters of SNModel described above
+            are distributed.
+
+"""
+import sncosmo
+import numpy as np
+
+
+class SNUniverse (object):
+
+    def __init__(self):
+        self.snIaModel = sncosmo.Model(source="salt2")
+        self._seed = None
+        self._z = None
+
+    @property
+    def seed(self):
+        return self._seed
+    def reset_model(self, seed, z):
+        self._seed = seed
+        self.set_z(z) 
+        return self.get_SNparams()
+
+    @property 
+    def z(self):
+        return self._z
+
+    def set_z(self,z ):
+        self._z = z
+        self.snIaModel.set(z=z)
+
+    def get_SNparams(self):
+
+        hundredyear = 100*365.0
+        np.random.seed(self.seed)
+        t0 = np.random.uniform(-hundredyear/2.0, hundredyear/2.0)
+        c = np.random.normal(0., 0.3)
+        x1 = np.random.normal(0., 1.0)
+        mabs = np.random.normal(-19.3, 0.3)
+        self.snIaModel.set(z=self.z, c=c, x1=x1)
+        self.snIaModel.set_source_peakabsmag(mabs, 'bessellb', 'ab')
+        x0 = self.snIaModel.get('x0')
+
+        return np.array([t0, x0, x1, c])
