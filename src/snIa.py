7d9baf92e940137508e54e346feed1003c53ee27
R. Biswas <rbiswas4@gmail.com>
2014-11-13 21:01:53 -0800

    dded the file CheckingLSST_bandpass.ipynb where I am trying to understand the conventions in the sims classes. A list of things I need to understand are: - Is sed.calcmag(bandpass) the way to go ? - What about FluxNorm ? - Why am I getting the error of non-overlapping wavelength after redshifting - How do I apply dust corrections? I attempted to use sed.apply_CCM dust but could not figure out what a_x, b_x were. I would have expected to be able to use a dustmap top obtain Av from RA, DEC. - What about plans for host galaxy extinction?

--- src/snIa.py
+++ src/snIa.py
@@ -12,81 +12,79 @@ from lsst.sims.catalogs.measures.instance import compound
 from lsst.sims.catalogs.generation.db import CatalogDBObject
 from lsst.sims.catalogs.generation.db import ObservationMetaData
 import sncosmo
-from astropy.cosmology import Planck13 as cosmo 
+from astropy.cosmology import Planck13 as cosmo
 
-#class SNIaCatalog (object):
+
+# class SNIaCatalog (object):
 class SNIaCatalog (InstanceCatalog):
     """
-    Supernova Type Ia in the catalog are characterized by the  following 
+    Supernova Type Ia in the catalog are characterized by the  following
     attributes
-    position (ra, dec, redshift), 
-    velocity wrt host galaxy, 
-    the supernova model (eg. SALT2) 
+    position (ra, dec, redshift),
+    velocity wrt host galaxy,
+    the supernova model (eg. SALT2)
     and parameters of the supernova model that predict the SED.
     """
-
-    column_outputs=['snid','snra','sndec', 'z','t0','c', 'x1', 'x0']
-    override_formats={'snra':'%8e','sndec':'%8e','c':'%8e','x0':'%8e'}
-    #column_outputs=['raJ2000','decJ2000','snid','z','snra', 'sndec', 'mass_stellar', 'c', 'x1', 't0', "x0"]
+    column_outputs = ['snid', 'snra', 'sndec', 'z', 't0', 'c', 'x1', 'x0']
+    override_formats = {'snra': '%8e', 'sndec': '%8e', 'c': '%8e', 'x0': '%8e'}
+# column_outputs=['raJ2000','decJ2000','snid','z','snra', 'sndec',\
+# 'mass_stellar', 'c', 'x1', 't0', "x0"]
 
     def get_snid(self):
         # Not necessarily unique if the same galaxy hosts two SN
         # rethink
-
         return self.column_by_name('id')
-    
+
     @property
     def numobjs(self):
         return len(self.column_by_name('id'))
-    
-    
+
     @compound('snra', 'sndec', 'z', 'vra', 'vdec', 'vr')
-    def get_angularCoordinates(self) :
-        _snra, _sndec, _z =  self.column_by_name('raJ2000'), \
-                             self.column_by_name('decJ2000'),\
-                             self.column_by_name('redshift')
-        _sndec += np.zeros(self.numobjs) 
-        _snra +=  np.zeros(self.numobjs) 
+    def get_angularCoordinates(self):
+        _snra, _sndec, _z = self.column_by_name('raJ2000'), \
+            self.column_by_name('decJ2000'), \
+            self.column_by_name('redshift')
+        _sndec += np.zeros(self.numobjs)
+        _snra += np.zeros(self.numobjs)
         _vra = np.zeros(self.numobjs)
         _vdec = np.zeros(self.numobjs)
         _vr = np.zeros(self.numobjs)
         return ([_snra, _sndec, _z, _vra, _vdec, _vr])
 
-    @compound( 'c', 'x1', 'x0', 't0')
-    def get_snparams(self) :
+    @compound('c', 'x1', 'x0', 't0')
+    def get_snparams(self):
         SNmodel = sncosmo.Model(source="salt2")
         hundredyear = 100*365.0
-        vals  = np.zeros(shape= (self.numobjs,4))
+        vals = np.zeros(shape=(self.numobjs, 4))
         _z, _id = self.column_by_name('redshift'), self.column_by_name('snid')
-        for i,v  in enumerate(vals) :
+        for i, v in enumerate(vals):
             np.random.seed(_id[i])
-            v[-1 ] = np.random.uniform(-hundredyear/2.0, hundredyear/2.0)
-            v[ 0 ] = np.random.normal(0., 0.3 )
-            v[ 1 ] = np.random.normal(0., 3.0 ) 
-            mabs = np.random.normal( -19.3, 0.3)
+            v[-1] = np.random.uniform(-hundredyear / 2.0, hundredyear / 2.0)
+            v[0] = np.random.normal(0., 0.3)
+            v[1] = np.random.normal(0., 3.0)
+            mabs = np.random.normal(-19.3, 0.3)
             SNmodel.set(z=_z[i], c=v[0], x1=v[1])
             SNmodel.set_source_peakabsmag(mabs, 'bessellb', 'ab')
-            v[2 ] = SNmodel.get('x0')
+            v[2] = SNmodel.get('x0')
+        print self.obs_metadata.mjd
+        print self.obs_metadata.bandpass
 
-
-        return  ([vals[:, 0 ], vals[:, 1], vals[:, 2], vals[:, 3]])
-
-if __name__=="__main__":
+        return ([vals[:, 0], vals[:, 1], vals[:, 2], vals[:, 3]])
+  
+if __name__ == "__main__":
 
     import lsst.sims.catUtils.baseCatalogModels as bcm
-    #import timeit
+    # import timeit
     print bcm.__file__
     from lsst.sims.catalogs.generation.db import ObservationMetaData
-    galDB = CatalogDBObject.from_objid( 'galaxyTiled' )
+    galDB = CatalogDBObject.from_objid('galaxyTiled')
+    myMJD  = 570123.15
+    
     myObsMD = ObservationMetaData(boundType='circle',
                                   unrefractedRA=5.0,
                                   unrefractedDec=15.0,
-                                  boundLength=0.1)
-    catalog = SNIaCatalog( db_obj=galDB, obs_metadata=myObsMD)
-    catalog.write_catalog("SNIaCat.txt")
-
-    
-
-
-
-
+                                  boundLength=3.5,
+                                  bandpass='u',
+                                  mjd=myMJD)
+    catalog = SNIaCatalog(db_obj=galDB, obs_metadata=myObsMD)
+#    #catalog.write_catalog("SNIaCat.txt")
