4126653d16cc914af0125daf4c4c0b885e780e9b
R. Biswas <rbiswas4@gmail.com>
2014-12-15 13:34:30 -0800

    test_data/standard_lc.dat: standard lc data file created by commented commands in testsnObject.py. testsnObject.py: generates a light curve for a SN, which was generated and saved as test_data/standard_lc.dat. This function asserts that the results must match.

--- tests/testsnObject.py
+++ tests/testsnObject.py
@@ -6,42 +6,38 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 
-from astropy.coordinates import SkyCoord
-
-import sncosmo
-from sncosmo import Model
-
 from lsst.sims.photUtils.Photometry import PhotometryStars, Sed, Bandpass
-from lsst.sims.photUtils.EBV import EBVbase
 from lsst.sims.photUtils.Photometry import PhotometryBase
 from sniacatalogs.snObject import SNObject
 
 import test_utils as tu
+# from unittest import assertAlmostEqual
 ra = 204.
 dec = -30.
 SN = SNObject(ra, dec)
 SN.set(x0=1.847e-6, x1=0.1, c=0., z=0.2)
-print SN
-SNCosmoSN = SNObject(ra, dec)
-SNCosmoSN.set(x0=1.847e-6, x1=0.1, z=0.2, mwebv=SN._mwebv)
 
-thisshouldbeNone = tu.getlsstbandpassobjs(loadcatsim=False, plot=True)
 bandPassList = ['u', 'g', 'r', 'i', 'z', 'y']
-sncosmobands = ['LSSTu', 'LSSTg', 'LSSTr', 'LSSTi', 'LSSTz', 'LSSTy']
 photometry = PhotometryBase()
 photometry.loadBandPassesFromFiles(bandPassList)
 lsstbands = photometry.bandPassList
-print type(lsstbands)
-w = sncosmo.get_bandpass(sncosmobands[0]).wave
 l = []
 for time in np.arange(-20., 50., 1.0):
-    t = time*np.ones(len(sncosmobands))
+    t = time*np.ones(len(bandPassList))
     t.tolist()
     x = SN.lsstbandmags(lsstbands, time=time)
-    y = SNCosmoSN.bandmag(band=sncosmobands, time=t, magsys='ab')
+    # y = SNCosmoSN.bandmag(band=sncosmobands, time=t, magsys='ab')
     e = [time]
     e += x.tolist()
-    e += y.tolist()
+    # e += y.tolist()
     l.append(e)
-header = "time(mjd) u g r i z y su sg sr si sz sy"
-np.savetxt('test_data/lc.dat', np.array(l), fmt='%10.6f', header=header)
+header = "time(mjd) u g r i z y"
+lc = np.array(l)
+
+# The 'test_data/standard_lc.dat' file was written using the following command
+#     for a SN at ra  = 204., -30. , with x0 = 1.846e-6, x1=0.1, c=0., z=0.2
+#     using the SALT2-extended model
+# np.savetxt('test_data/standard_lc.dat', np.array(l), fmt='%10.6f', header=header)
+std_lc = np.loadtxt('test_data/standard_lc.dat')
+np.testing.assert_allclose(std_lc, lc)
+#ass
