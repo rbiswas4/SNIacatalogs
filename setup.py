5f713f1c6be3e9cba0d01ad332163c2908ff0a3d
R. Biswas <rbiswas4@gmail.com>
2014-12-15 10:56:57 -0800

    Added setup so that accessible from elsewhere

---
+++ setup.py
@@ -0,0 +1,10 @@
+#from ez_setup import use_setuptools
+#use_setuptools()
+from setuptools import setup, find_packages
+
+setup(name="SNIaCatalogs",
+      version="0.1dev",
+      description='SNIaCatalogs produces a catalog of simulated SNIa associated to galaxies in a galaxy catalog',
+      long_description=''' ''',
+      packages=['sniacatalogs']
+      )
