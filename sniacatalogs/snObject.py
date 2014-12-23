510016bc7a100e4cdfc325e57577866091fd49d6
R. Biswas <rbiswas4@gmail.com>
2014-12-22 19:16:24 -0800

    testsnObject.py: Not sure what the change is. Will remove, because the     useful parts have been put into testSNIaFunctionality.py snObject.py: No real change. Commented out parts are removed.

--- sniacatalogs/snObject.py
+++ sniacatalogs/snObject.py
@@ -25,68 +25,6 @@ dustmaproot = os.getenv('SIMS_DUSTMAPS_DIR')
 map_dir = os.path.join(dustmaproot, 'DustMaps')
 
 wavelenstep = 0.1
-plot = False
-
-
-# def getlsstbandpassobjs(loadsncosmo=True, loadcatsim=True):
-#     """
-#     General utility to return a list of the baseline LSST bandpasses loaded
-#     as catsim bandpass objects, and register them as SNCosmo bandpasses
-#     accessible through strings like 'LSSTu'.
-#     args:
-#         loadsncosmo: Bool, optional, defaults to True
-#             variable to decide whether to register the LSST bandpasses as
-#             SNCosmo registered bandpass objects accessible through strings
-#             like 'LSSTu'
-#         loadcatsim : Bool, optional, defaults to True
-#             variable to decide whether to set up catsim bandpass objects
-#             are return the list of u,g,r,i,z,y bandpasses
-#     returns:
-#         if loadcatsim is true, list of catsim bandpass objects corresponding
-#         to LSST baseline u, g, r, i, z, y filters.
-#         if loadcatsim is False, return is None
-# 
-#     Examples:
-# 
-#     """
-#     bandPassList = ['u', 'g', 'r', 'i', 'z', 'y']
-#     banddir = os.path.join(os.getenv('THROUGHPUTS_DIR'), 'baseline')
-#     lsstbands = []
-#     lsstbp = {}
-# 
-#     for band in bandPassList:
-#         # setup sncosmo bandpasses
-#         bandfname = banddir + "/total_" + band + '.dat'
-#         if loadsncosmo:
-#             # register the LSST bands to the SNCosmo registry
-#             # Not needed for LSST, but useful to compare independent codes
-#             # Usually the next two lines can be merged,
-#             # but there is an astropy bug currently which affects only OSX.
-#             numpyband = np.loadtxt(bandfname)
-#             sncosmoband = sncosmo.Bandpass(wave=numpyband[:, 0],
-#                                            trans=numpyband[:, 1],
-#                                            wave_unit=Unit('nm'),
-#                                            name='LSST' + band)
-#             sncosmo.registry.register(sncosmoband, force=True)
-#         if loadcatsim:
-#             # Now load LSST bandpasses for catsim
-#             lsstbp[band] = Bandpass()
-#             lsstbp[band].readThroughput(bandfname, wavelen_step=wavelenstep)
-#             lsstbands.append(lsstbp[band])
-#     fifilterfigs, filterax = plt.subplots()
-#     if plot:
-#         for band in bandPassList:
-#             b = sncosmo.get_bandpass('LSST' + band)
-#             filterax.plot(b.wave, b.trans, '-k', lw=2.0)
-#             filterax.set_xlabel(r'$\lambda$, ($\AA$)')
-#             filterax.set_ylabel(r'transmission')
-#         plt.show()
-# 
-#     if loadcatsim:
-#         return lsstbands
-#     else:
-#         return None
-
 
 class SNObject (Model):
     """
--- tests/testsnObject.py
+++ tests/testsnObject.py
@@ -3,15 +3,18 @@ Test the simulation of a SNIa light curve using snObject at specified ra, dec,
 and specific SN (SALT2) parameters. 
 """
 import os
-import numpy as np
+import numpy
+import unittest
 import matplotlib.pyplot as plt
 
 from lsst.sims.photUtils.Photometry import PhotometryStars, Sed, Bandpass
 from lsst.sims.photUtils.Photometry import PhotometryBase
 from snObject import SNObject
+import lsst.utils.tests as utilsTests
 
 import utils_for_test as tu
 # from unittest import assertAlmostEqual
+
 ra = 204.
 dec = -30.
 SN = SNObject(ra, dec)
@@ -22,8 +25,8 @@ photometry = PhotometryBase()
 photometry.loadBandPassesFromFiles(bandPassList)
 lsstbands = photometry.bandPassList
 l = []
-for time in np.arange(-20., 50., 1.0):
-    t = time*np.ones(len(bandPassList))
+for time in numpy.arange(-20., 50., 1.0):
+    t = time*numpy.ones(len(bandPassList))
     t.tolist()
     x = SN.bandMags(lsstbands, time=time)
     # y = SNCosmoSN.bandmag(band=sncosmobands, time=t, magsys='ab')
@@ -32,13 +35,12 @@ for time in np.arange(-20., 50., 1.0):
     # e += y.tolist()
     l.append(e)
 header = "time(mjd) u g r i z y"
-lc = np.array(l)
-np.savetxt('testData/lc.dat', lc, fmt='%10.6f', header=header)
+lc = numpy.array(l)
+numpy.savetxt('testData/lc.dat', lc, fmt='%10.6f', header=header)
 
 # The 'testData/standard_lc.dat' file was written using the following command
 #     for a SN at ra  = 204., -30. , with x0 = 1.846e-6, x1=0.1, c=0., z=0.2
 #     using the SALT2-extended model
-# np.savetxt('testData/standard_lc.dat', np.array(l), fmt='%10.6f', header=header)
-std_lc = np.loadtxt('testData/standard_lc.dat')
-np.testing.assert_allclose(std_lc, lc)
-#ass
+# numpy.savetxt('testData/standard_lc.dat', numpy.array(l), fmt='%10.6f', header=header)
+std_lc = numpy.loadtxt('testData/standard_lc.dat')
+numpy.testing.assert_allclose(std_lc, lc)
