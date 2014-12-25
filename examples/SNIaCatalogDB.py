f1d911aea5fd429ca6cd253ae4a961e189a7e9f7
R. Biswas <rbiswas4@gmail.com>
2014-12-25 13:18:38 -0800

    Split the example into two functions, the first one writes the catalog to text ASCII files, reads the ASCII file and write to a DB. The second function converts the DB into a pandas dataframe, groups observations to groups involving the same SN by ID, and writes out light curves to ASCII files.

--- examples/SNIaCatalogDB.py
+++ examples/SNIaCatalogDB.py
@@ -35,7 +35,6 @@ from lsst.sims.catalogs.generation.db import ObservationMetaData
 
 
 
-galDB = CatalogDBObject.from_objid('galaxyTiled')
 
 
 def _file2lst(fname, i, mjd):
@@ -48,13 +47,27 @@ def _file2lst(fname, i, mjd):
     return l
 # main example :  create all the 'observations' separated by a day, 
 
-myMJDS = [570123.15 + 3.*i for i in range(20)]
 
-createcat = False
-connection = sqlite3.connect('data/sncat.db')
-if createcat:
+def writeCatalogtoDB(dbfile, dbtable, ascii_root):
+    '''
+    Write a set of instance catalogs to a sqlite3 database file called db_fname,
+    deleting the file if it already exists. This is done in two steps: first the
+    write method of instance catalogs is used to write to ASCII files that are
+    not removed, and then these are read into the database. The ASCII files 
+    are not removed. It is assumed that the directory in which these files 
+    are written to exist.
+    '''
+    # Setup connection to write
+    connection = sqlite3.connect(dbfile)
     curs = connection.cursor()
-    curs.execute('CREATE TABLE if not exists mysncat (id TEXT, mjd FLOAT, snid INT, snra FLOAT, sndec FLOAT, z FLOAT, t0 FLOAT, c FLOAT, x1 FLOAT, x0 FLOAT, mag_u FLOAT, mag_g FLOAT, mag_r FLOAT, mag_i FLOAT, mag_z FLOAT, mag_y FLOAT)')
+    curs.execute('CREATE TABLE if not exists mysncat \
+            (id TEXT, mjd FLOAT, snid INT, snra FLOAT, sndec FLOAT,\
+            z FLOAT, t0 FLOAT, c FLOAT, x1 FLOAT, x0 FLOAT,\
+            mag_u FLOAT, mag_g FLOAT, mag_r FLOAT, mag_i FLOAT,\
+            mag_z FLOAT, mag_y FLOAT)')
+    # Catalog, and range over which it is written 
+    galDB = CatalogDBObject.from_objid('galaxyTiled')
+    myMJDS = [570123.15 + 3.*i for i in range(2)]
     for i, myMJD in enumerate(myMJDS):
         myObsMD = ObservationMetaData(boundType='circle',
                                      unrefractedRA=5.0,
@@ -67,24 +80,30 @@ if createcat:
         print "====================================="
         print i, type(catalog.usedlsstbands()) , catalog.obs_metadata.mjd
         print "====================================="
-        fname = "data/SNIaCat_" + str(i) + ".txt"
+        # fname = "data/SNIaCat_" + str(i) + ".txt"
+        fname = ascii_root + str(i) + ".txt"
         catalog.write_catalog(fname)
         l = _file2lst(fname, i, mjd=myMJD)
         recs = sq.array2dbrecords(l)
-        exec_str = sq.insertfromdata(tablename='mysncat', records=recs, multiple=True)
+        exec_str = sq.insertfromdata(tablename=dbtable, records=recs,
+                                     multiple=True)
         curs.executemany(exec_str, recs)
     connection.commit()
-df = pd.read_sql('SELECT * FROM mysncat', connection)
-connection.close()
+    connection.close()
 
-def getLCsFromDF(df):
+def getLCsFromDB(dbfile, dbtable, lc_root):
+    connection = sqlite3.connect(dbfile)
+    df = pd.read_sql('SELECT * FROM ' + dbtable, connection)
     grouped = df.groupby('snid')
     snids = grouped.groups.keys()
     for snid in snids:
-        fname =  'data/LightCurves/SN' + str(snid) + '.dat'
+        fname =  lc_root + str(snid) + '.dat'
         mylc = df.loc[grouped.groups[snid]]
-        mylc.to_csv(fname, na_rep = "NaN")
-getLCsFromDF(df)
+        mylc.to_csv(fname, na_rep = "NaN", index=False)
+    connection.close()
+if __name__=="__main__":
+    writeCatalogtoDB(dbfile='data/sncat.db', dbtable='mysncat', ascii_root='data/SNIaCat_')
+    getLCsFromDB(dbfile='data/sncat.db', dbtable='mysncat', lc_root='data/LightCurves/SN_')
 
 
 
