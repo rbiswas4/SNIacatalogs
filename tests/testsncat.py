42a8e55c8b1ed71c0ee9f68f4f4623b0323134f7
R. Biswas <rbiswas4@gmail.com>
2015-03-27 21:32:10 -0700

    Adding testsncat as unittest for SN catalogs. Not working yet

--- tests/testsncat.py
+++ tests/testsncat.py
@@ -1,111 +1,149 @@
-#!/usr/bin/env python
-
-from cStringIO import StringIO
-import sys
+"""
+Test the simulation of a SNIa light curve using snObject at specified ra, dec,
+and specific SN (SALT2) parameters.
+"""
 import os
-import matplotlib as mpl
-mpl.use('Agg')
+import numpy
+import unittest
 import matplotlib.pyplot as plt
-import numpy as np
-import lsst.sims.catUtils.baseCatalogModels as bcm
-from lsst.sims.catalogs.measures.instance import InstanceCatalog
-from lsst.sims.catalogs.measures.instance import compound
-from lsst.sims.catalogs.generation.db import CatalogDBObject
-from lsst.sims.catalogs.generation.db import ObservationMetaData
-from lsst.sims.photUtils import Sed
-import lsst.sims.photUtils.Bandpass as Bandpass
-import sncosmo
-from astropy.units import Unit
-import astropy.cosmology as cosmology 
-#from astropy.cosmology import Planck13 as cosmo
+
+from lsst.sims.photUtils.Photometry import PhotometryStars, Sed, Bandpass
+from lsst.sims.photUtils.Photometry import PhotometryBase
+import lsst.utils.tests as utilsTests
+
 from snObject import SNObject
 from sncat import SNIaCatalog
-from lsst.sims.photUtils.CosmologyObject import CosmologyWrapper 
-#import sqliteutils as sq
-import testUtilsSNe as sq
-import sqlite3
-import pandas as pd
-wavelenstep = 0.1
+import utils_for_test as tu
 
-import lsst.sims.catUtils.baseCatalogModels as bcm
-# import timeit
-print bcm.__file__
+from astropy.units import Unit
+from astropy.coordinates import SkyCoord
+from sncosmo import Model
+import sncosmo
+
+import eups
+from lsst.sims.catalogs.measures.instance import InstanceCatalog
+# from lsst.sims.catalogs.measures.instance import compound
+from lsst.sims.catalogs.generation.db import CatalogDBObject
 from lsst.sims.catalogs.generation.db import ObservationMetaData
 
+import testUtilsSNe as sq
+import sqlite3
+# Global Variables for calculating MWEBV through astropy/SNcosmo
+dustmaproot = eups.productDir('sims_dustmaps')
+map_dir = os.path.join(dustmaproot, 'DustMaps')
 
-# Define the cosmology for the catalog
-cosmo = CosmologyWrapper() 
-# You can set the cosmology w0waCDM FLRW cosmologies
-cosmo.setCosmology(Om0=0.25, Ok0=None, H0=73.0)
 
-galDB = CatalogDBObject.from_objid('galaxyTiled')
-def file2lst(fname, i, mjd):
-    d = np.loadtxt(fname, delimiter=',')
+def _file2lst(fname, i, mjd):
+    d = numpy.loadtxt(fname, delimiter=',')
     l = list()
     for i, row in enumerate(d):
         obsid = 'obshist' + str(i)
         lst = [obsid] + [mjd] + row.tolist()
         l.append(lst)
     return l
-# main example :  create all the 'observations' separated by a day, 
-
-myMJDS = [570123.15 + 3.*i for i in range(20)]
-
-createcat = True
-#prepare a new sncat table:
-#delete catalog if it exists manually
-connection = sqlite3.connect('testData/sncat.db')
-# connection = sqlite3.connect('testData/STDsncat.db')
-curs = connection.cursor()
-curs.execute('CREATE TABLE if not exists mysncat (id TEXT, mjd FLOAT, snid INT, snra FLOAT, sndec FLOAT, z FLOAT, t0 FLOAT, c FLOAT, x1 FLOAT, x0 FLOAT, mag_u FLOAT, mag_g FLOAT, mag_r FLOAT, mag_i FLOAT, mag_z FLOAT, mag_y FLOAT)')
-for i, myMJD in enumerate(myMJDS):
-    if createcat:
-        myObsMD = ObservationMetaData(boundType='circle',
-                                      unrefractedRA=5.0,
-                                      unrefractedDec=15.0,
-                                      boundLength=0.015,
-                                      bandpassName=['u', 'g', 'r', 'i',
-                                                    'z', 'y'],
-                                      mjd=myMJD)
-        catalog = SNIaCatalog(db_obj=galDB,
-                              obs_metadata=myObsMD)
-        print "====================================="
-        print i, type(catalog.usedlsstbands()) , catalog.obs_metadata.mjd
-        print "====================================="
-        fname = "testData/SNIaCat_" + str(i) + ".txt"
-        catalog.write_catalog(fname)
-    fname = "testData/SNIaCat_" + str(i) + ".txt"
-    l = file2lst(fname, i, mjd=myMJD)
-    recs = sq.array2dbrecords(l)
-    exec_str = sq.insertfromdata(tablename='mysncat', records=recs, multiple=True)
-    curs.executemany(exec_str, recs)
-connection.commit()
-curs.execute('SELECT * FROM mysncat')  
-dblcs = curs.fetchall()
-print 'In Database: ', type(dblcs)
-connection.close()
-
-stddata = np.loadtxt('testData/SNIaCat_0_std.txt', delimiter=',')
-newdata = np.loadtxt('testData/SNIaCat_0.txt', delimiter=',')
-# import pandas as pd
-# stddata = pd.DataFrame.from_csv('testData/SNIaCat_0_std.txt')
-# newdata = pd.DataFrame.from_csv('testData/SNIaCat_0.txt')
-std = stddata.sort(axis=0)
-new = newdata.sort(axis=0)
-# Need to understand how to match nans
-def nptest():
-    try:
-        np.testing.assert_allclose(stddata, newdata)
-        print "test passed"
-    except:
-        print 'test failed' 
-    return
-def test() :
-    if new.equals(std):
-        print 'pass'
-    else:
-        print 'Fail'
-nptest()
 
 
+class testSNIaCatalog(unittest.TestCase):
+    """
+    Unit tests to test the functionality of the SNIaCatalog class.
 
+    Requires:
+    ---------
+        connection to LSST database
+
+
+    Tests:
+    ------
+        Write out an instance catalog of SNIa
+        Find SNIa 'observed' according to obs_metadata associated\
+        with an LSST view, catalog in an instance catalog and write to ascii\
+        files testData/SNIaCat_i.txt as output.
+        testICatOuput:
+        testWriteToSQLite:
+        testLCFromSQLite:
+    """
+    mjds = [570123.15 + 3.*i for i in range(2)]
+
+    @classmethod
+    def setUpClass(cls):
+        # delete previous test db if present
+        if os.path.exists('testData/sncat.db'):
+            print 'deleting previous database'
+            os.unlink('testData/sncat.db')
+
+        mjds = [570123.15 + 3.*i for i in range(2)]
+        galDB = CatalogDBObject.from_objid('galaxyTiled')
+
+        for i, myMJD in enumerate(mjds):
+            myObsMD = ObservationMetaData(boundType='circle',
+                                          boundLength=0.015,
+                                          unrefractedRA=5.0,
+                                          unrefractedDec=15.0,
+                                          bandpassName=
+                                          ['u', 'g', 'r', 'i', 'z', 'y'],
+                                          mjd=myMJD)
+            catalog = SNIaCatalog(db_obj=galDB, obs_metadata=myObsMD)
+            fname = "testData/SNIaCat_" + str(i) + ".txt"
+            print fname, myObsMD.mjd
+            catalog.write_catalog(fname)
+
+    @classmethod
+    def tearDownClass(cls):
+        pass
+        # delete the test db
+        # if os.path.exists('testData/sncat.db'):
+        #    print 'deleting previous database'
+        #    os.unlink('testData/sncat.db')
+
+    def testICatOutput(self):
+        """
+        Check that the output of the instance catalog SNIaCatalog
+
+        """
+        stddata = numpy.loadtxt('testData/SNIaCat_0_std.txt', delimiter=',')
+        newdata = numpy.loadtxt('testData/SNIaCat_0.txt', delimiter=',')
+        stddata.sort(axis=0)
+        newdata.sort(axis=0)
+        numpy.testing.assert_allclose(stddata, newdata, rtol=1.0e-3)
+
+    def testWriteToSQLite(self):
+        """
+        """
+        connection = sqlite3.connect('testData/sncat.db')
+        curs = connection.cursor()
+        curs.execute('CREATE TABLE if not exists mysncat (id TEXT, mjd FLOAT, snid INT, snra FLOAT, sndec FLOAT, z FLOAT, t0 FLOAT, c FLOAT, x1 FLOAT, x0 FLOAT, mag_u FLOAT, mag_g FLOAT, mag_r FLOAT, mag_i FLOAT, mag_z FLOAT, mag_y FLOAT)')
+
+        for i, myMJD in enumerate(self.mjds):
+            fname = "testData/SNIaCat_" + str(i) + ".txt"
+            l = _file2lst(fname, i, mjd=myMJD)
+            recs = sq.array2dbrecords(l)
+            exec_str = sq.insertfromdata(tablename='mysncat', records=recs,
+                                         multiple=True)
+            curs.executemany(exec_str, recs)
+        connection.commit()
+        # connection.close()
+        # def testLCFromSQLite(self):
+        # """
+        # """
+        # connection = sqlite3.connect('testData/sncat.db')
+        # curs = connection.cursor()
+        curs.execute('SELECT * FROM mysncat')
+        print 'LC In Database: '
+        lc = curs.fetchall()
+        print type(lc)
+
+
+
+
+def suite():
+    utilsTests.init()
+    suites = []
+    suites += unittest.makeSuite(testSNIaCatalog)
+    return unittest.TestSuite(suites)
+
+
+def run(shouldExit=False):
+    utilsTests.run(suite(), shouldExit)
+
+if __name__ == "__main__":
+    run(shouldExit=True)
