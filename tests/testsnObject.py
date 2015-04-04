e880f67fe0d99f1f7ac9fd2770161a963ad3391e
R. Biswas <rbiswas4@gmail.com>
2015-04-03 17:56:22 -0700

    Got testsnObject.py working in all three tests! Changed the ra, dec values to old values so that testing against the old SN light curve works

--- tests/testsnObject.py
+++ tests/testsnObject.py
@@ -57,32 +57,22 @@ class testSNObject(unittest.TestCase):
         Store objects:
 
         # NO MW Extinction objects from catsim and SNCosmo
-        self.SNnomw : SN with said parameters, unexincted in MW
+        self.SNnoMW : SN with said parameters, unexincted in MW
         self.SNCosmo_nomw :
 
         # objects with MW extinction
         self.SNmw : SN with said parameters, exincted in MW
         self.SNCosmo_mw :
         """
-        ra = 8.723553e-02 
-        dec = 2.618648e-01
+        ra = 204.
+        dec = -30.
         mjdobs = 571203.
         # Setup SN object of different kinds we want to check
-        
-        
-
-
-        # SNObject with ra, dec, whose extinction will be calculated
-        # separately
-
-        # Use catsim to calculate SN light curve properties from ra, dec
-        # and parameters. Do not use MW, LC properties are then really
-        # independent of RA DEC. Store in self.SNmw
 
         # In order to compare SN light curves between SNCosmo and Catsim
         # we need to set them up separately
 
-
+            # Check case with no MW extinction
         SNnoMW = SNObject()
         SNnoMW.set(x0=1.847e-6, x1=0.1, c=0., z=0.2)
         SNnoMW.set_MWebv(0.)
@@ -103,7 +93,6 @@ class testSNObject(unittest.TestCase):
         SN.set(x0=1.847e-6, x1=0.1, c=0., z=0.2)
         SNnoMW.set_MWebv(0.)
         self.SNmw = SN
-        print SN.ebvofMW
 
         SNCosmo = sncosmo.Model(source='salt2-extended',
                                 effects=[dust, dust],
@@ -121,12 +110,14 @@ class testSNObject(unittest.TestCase):
 
 
         # WHAT THE HELL is SNCosmo_float?
+        CCMdust = sncosmo.CCM89Dust()
         SNCosmo_float = sncosmo.Model(source='salt2-extended',
-                                      effects=[dust, dust],
+                                      effects=[CCMdust, CCMdust],
                                       effect_names=['host', 'mw'],
                                       effect_frames=['rest', 'obs'])
 
         SNCosmo_float.set(x0=1.847e-6, x1=0.1, c=0., z=0.2)
+        SNCosmo_float.set(mwebv=t_mwebv)
         self.SNCosmo_float = SNCosmo_float
         
                 # Load LSST sofware bandpass objects for magnitude calculation
@@ -136,8 +127,8 @@ class testSNObject(unittest.TestCase):
         pbase.setupPhiArray_dict()
         self.pbase = pbase
         self.lsstbands = pbase.bandpassDict
-        #self.times = numpy.arange(-20., 50., 1.0)
-        self.times = numpy.array([0.])
+        self.times = numpy.arange(-20., 50., 1.0)
+        # self.times = numpy.array([0.])
 
         # Load SNCosmo bandpass objects for comparison test
         thisshouldbeNone = tu.getlsstbandpassobjs(self.bandPassList,
@@ -163,7 +154,6 @@ class testSNObject(unittest.TestCase):
                                                                 phiarray=self.pbase.phiArray).\
                                                                 tolist()
             e = [time]
-            # e  += bandMagsfromLSST.tolist()
             lsst.append(bandMagsfromLSST)
 
             t = time*numpy.ones(len(self.bandPassList))
@@ -210,103 +200,36 @@ class testSNObject(unittest.TestCase):
             numpy.testing.assert_allclose(numpy.array(sncosmo),
                                           numpy.array(lsst))
 
+    def testSNObject_lc_againstprev(self):
+         """
+         write out the light curve of a single SN represented as SNObject over a
+         number of days, and check that the results are the same as the results
+         before stored in testData/std_lc.dat
+         """
+ 
+         l = []
+         for time in self.times:
+             # for time in numpy.arange(-20., 50., 1.0):
+             x = self.SNmw.catsimBandMags(time=time, bandpassobject=self.lsstbands,
+                                          phiarray=self.pbase.phiArray)
 
-###     def testSNmwebv(self):
-###         """
-###         The mwebv parameter is found from dustmaps and the coordinates
-###         (ra, dec) in both the catsim and SNCosmo (astropy) framework. Check
-###         that these values match to a certain precision
-###         """
-###         return
-###         sncosmoval = self.SNCosmo_mw.parameters[-2]
-###         lsstval = self.SNmw.ebvofMW
-###         numpy.testing.assert_allclose(sncosmoval, lsstval)
-### 
-###     def testSNObjectMWmags_SNCosmowithsamemwebv(self):
-###         """
-###         If we use the same mwebv parameter in both the LSST catsim and SNCosmo
-###         implementation of O'Donnell 94, check for differences in the band
-###         magnitudes
-###         """
-###         return
-### 
-###         lsstval = self.SNmw.ebvofMW
-###         self.SNCosmo_float.set(mwebv=lsstval)
-### 
-###         lsst = []
-###         sncosmo = []
-###         for time in self.times:
-### 
-###             bandMagsfromLSST = self.SNmw.bandMags(self.lsstbands, time=time)
-###             e = [time]
-###             # e  += bandMagsfromLSST.tolist()
-###             lsst.append(bandMagsfromLSST.tolist())
-### 
-###             t = time*numpy.ones(len(self.bandPassList))
-###             t.tolist()
-###             z = [time]
-###             y = self.SNCosmo_float.bandmag(band=self.sncosmobands, time=t,
-###                                            magsys='ab')
-###             # z += y.tolist()
-###             sncosmo.append(y.tolist())
-###             numpy.testing.assert_allclose(numpy.array(sncosmo),
-###                                           numpy.array(lsst))
-### 
-###     def testSNObjectMWmags_SNCosmo(self):
-###         """
-###         magnitude calculation for SNObject with extinction:
-###         Compare the lsst band magnitudes computed using SNCosmo and the LSST
-###         software stack. This is done by using the SNObject method bandMags and
-###         compared with the SNCosmo.Model method bandmags with an SNObject whose
-###         mwebv attribute is calculated in different ways.
-###         """
-###         lsst = []
-###         sncosmo = []
-###         for time in self.times:
-### 
-###             bandMagsfromLSST = self.SNmw.bandMags(self.lsstbands, time=time)
-###             e = [time]
-###             # e  += bandMagsfromLSST.tolist()
-###             lsst.append(bandMagsfromLSST.tolist())
-### 
-###             t = time*numpy.ones(len(self.bandPassList))
-###             t.tolist()
-###             z = [time]
-###             y = self.SNCosmo_mw.bandmag(band=self.sncosmobands, time=t,
-###                                         magsys='ab')
-###             # z += y.tolist()
-###             sncosmo.append(y.tolist())
-###             numpy.testing.assert_allclose(numpy.array(sncosmo),
-###                                           numpy.array(lsst))
-### 
-###     def testSNObject_lc_againstprev(self):
-###         """
-###         write out the light curve of a single SN represented as SNObject over a
-###         number of days, and check that the results are the same as the results
-###         before stored in testData/std_lc.dat
-###         """
-### 
-###         l = []
-###         for time in self.times:
-###             # for time in numpy.arange(-20., 50., 1.0):
-###             x = self.SNmw.bandMags(self.lsstbands, time=time)
-###             # y = SNCosmoSN.bandmag(band=sncosmobands, time=t, magsys='ab')
-###             e = [time]
-###             e += x.tolist()
-###             # e += y.tolist()
-###             l.append(e)
-###         header = "time(mjd) u g r i z y"
-###         lc = numpy.array(l)
-###         numpy.savetxt('testData/lc.dat', lc, fmt='%10.6f', header=header)
-### 
-###         # The 'testData/standard_lc.dat' file was written using the following
-###         # for a SN at ra  = 204., -30. ,
-###         # with x0 = 1.846e-6, x1=0.1, c=0., z=0.2
-###         # using the SALT2-extended model
-###         # numpy.savetxt('testData/standard_lc.dat', numpy.array(l),
-###         #               fmt='%10.6f', header=header)
-###         std_lc = numpy.loadtxt('testData/standard_lc.dat')
-###         numpy.testing.assert_allclose(std_lc, lc)
+             # y = self.SNCosmo_float.bandmag(band=self.sncosmobands, time=time, magsys='ab')
+             e = [time]
+             e += x.tolist()
+             # e += y.tolist()
+             l.append(e)
+         header = "time(mjd) u g r i z y"
+         lc = numpy.array(l)
+         numpy.savetxt('testData/lc.dat', lc, fmt='%10.6f', header=header)
+ 
+         # The 'testData/standard_lc.dat' file was written using the following
+         # for a SN at ra  = 204., -30. ,
+         # with x0 = 1.846e-6, x1=0.1, c=0., z=0.2
+         # using the SALT2-extended model
+         # numpy.savetxt('testData/standard_lc.dat', numpy.array(l),
+         #               fmt='%10.6f', header=header)
+         std_lc = numpy.loadtxt('testData/standard_lc.dat')
+         numpy.testing.assert_allclose(std_lc, lc)
 
 
 def suite():
